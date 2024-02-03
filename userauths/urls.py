59aa6c2f36d18644a06203146d4f1eadf259247c
officialjwise <officialjwise20@gmail.com>
2024-02-03 20:36:47 +0000

    required urls passed

--- userauths/urls.py
+++ userauths/urls.py
@@ -4,5 +4,8 @@ from userauths import views
 app_name = "userauths"
 
 urlpatterns = [
-    path("register/", views.RegisterView, name="register")
+    path("register/", views.RegisterView, name="register"),
+    path("login/", views.loginView, name="login"),
+    path("logout/", views.logoutView, name="logout"),
+
 ]
