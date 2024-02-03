79c1ee845337c909dcfa5c7bfd60f7a1e36fa47c
officialjwise <officialjwise20@gmail.com>
2024-02-03 20:34:37 +0000

    imported User from the userauths models instead of settings.AUTH_USER_MODEL

--- userauths/forms.py
+++ userauths/forms.py
@@ -4,11 +4,14 @@ from userauths.models import User
 
 
 class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "First Name"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Last Name"}))
     username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "Username"}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    # phoneNumber = forms.NumberInput(attrs={"placeholder": "Mobile Number"})
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}))
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
