3eeb933ee4a42006ed355dd7814353ffac171a47
officialjwise <officialjwise20@gmail.com>
2024-02-03 19:46:28 +0000

    Have been modified to allow both username and email for login authentication. Also determines if the entered data is username or email in that specific-context by using the @ to determine an email and throws the error message based on that.

--- userauths/views.py
+++ userauths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 def RegisterView(request):
@@ -22,3 +25,38 @@ def RegisterView(request):
         'form': form,
     }
     return render(request, "userauths/register.html", context)
+
+
+def loginView(request):
+    # Checking if the user is already authenticated
+    if request.user.is_authenticated:
+        return redirect("store:index")
+
+    if request.method == "POST":
+        email_or_username = request.POST.get("email_or_username")
+        password = request.POST.get("password")
+
+        # Using try and except block to filter the errors
+        try:
+            # Check if email_or_username contains "@" symbol
+            if '@' in email_or_username:
+                user = User.objects.get(email=email_or_username)
+            else:
+                user = User.objects.get(username=email_or_username)
+        except User.DoesNotExist:
+            messages.warning(request, f"No user found with {email_or_username}. Please check your credentials or register.")
+            return redirect("userauths:register")
+
+        # Authenticate using either email or username, not both
+        user = authenticate(request, username=user.username, password=password) or \
+               authenticate(request, email=user.email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"You're logged in!")
+            return redirect("store:index")
+        else:
+            messages.warning(request, f"Invalid login credentials.")
+
+    context = {}
+    return render(request, "userauths/login.html", context)
