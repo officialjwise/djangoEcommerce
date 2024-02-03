b816b095adb6f6ec453d8238047ff4db43d54021
officialjwise <officialjwise20@gmail.com>
2024-02-03 20:37:31 +0000

    First name and Last Name fields added.

--- userauths/admin.py
+++ userauths/admin.py
@@ -3,7 +3,7 @@ from userauths.models import User
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'bio']
+    list_display = ['username', 'first_name', 'last_name', 'email', 'bio']
 
 
 admin.site.register(User, UserAdmin)
