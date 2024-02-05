8009f3df83f9cd54d75b9717971923e3bf5b5083
officialjwise <officialjwise20@gmail.com>
2024-02-05 22:18:16 +0000

    Create models for product management
    - Add Category, Tags, Vendor, Product, ProductImages models.
    - Implement CartOrder, CartOrderItems, ProductReview, Wishlist, and Address models.
    - Define fields, relationships, and methods in each model for comprehensive product management.
    
    Add Product model with updated price field
    
    - Update the price field in the Product model to use max_digits and decimal_places for clarity.
    - Fix image upload issue by installing Pillow library.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,211 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.html import mark_safe
+from userauths.models import User
 
-# Create your models here.
+STATUS_CHOICE = (
+    ("process", "Processing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered"),
+)
+
+STATUS = (
+    ("draft", "Draft"),
+    ("disabled", "Disabled"),
+    ("rejected", "Rejected"),
+    ("in_review", "In Review"),
+    ("published", "Published"),
+)
+RATING = (
+    (1, "★☆☆☆☆"),
+    (2, "★★☆☆☆"),
+    (3, "★★★☆☆"),
+    (4, "★★★★☆"),
+    (5, "★★★★★"),
+)
+
+
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+# Shop Categories
+class Category(models.Model):
+    category_id = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefgh123456")
+    title = models.CharField(max_length=100, default="Food")
+    image = models.ImageField(upload_to="category", default="category.jpg")
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    # Display categories image in the admin panel
+    def category_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % self.image.url)
+
+    # String representation in the database
+    def __str__(self):
+        return self.title
+
+
+class Tags(models.Model):
+    pass
+
+
+# Vendor model
+
+class Vendor(models.Model):
+    vendor_id = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefgh123456")
+
+    title = models.CharField(max_length=100, default="Carisca Store")
+    image = models.ImageField(upload_to=user_directory_path, default="vendor.jpg")
+    description = models.TextField(null=True, blank=True, default="I am an amazing vendor")
+
+    address = models.CharField(max_length=100, default="123 Kotei")
+    contact = models.CharField(max_length=100, default="+233 543482189")
+    shipping_on_time = models.CharField(max_length=100, default="100")
+    authentic_rating = models.CharField(max_length=100, default="100")
+    days_return = models.CharField(max_length=100, default="100")
+    warranty_period = models.CharField(max_length=100, default="100")
+    chat_response_time = models.CharField(max_length=100, default="100")
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name_plural = "Vendors"
+
+    # Display categories image in the admin panel
+    def vendor_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % self.image.url)
+
+    # String representation in the database
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    product_id = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefgh123456")
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    title = models.CharField(max_length=100, default="Fresh Pear")
+    image = models.ImageField(upload_to=user_directory_path, default="product.jpg")
+    description = models.TextField(null=True, blank=True, default="This is the product")
+
+    price = models.DecimalField(max_digits=10, decimal_places=2, default="1.99")
+    old_price = models.DecimalField(max_digits=10, decimal_places=2, default="2.99")
+
+    specifications = models.TextField(null=True, blank=True)
+    tags = models.ForeignKey(Tags, on_delete=models.SET_NULL, null=True)
+
+    product_status = models.CharField(choices=STATUS, max_length=10, default="in_review")
+
+    status = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    digital = models.BooleanField(default=False)
+
+    sku = ShortUUIDField(unique=True, length=4, max_length=10, prefix="sku", alphabet="abcdefgh123456")
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(null=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    # Display categories image in the admin panel
+    def product_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % self.image.url)
+
+    # String representation in the database
+    def __str__(self):
+        return self.title
+
+    def get_discount_percentage(self):
+        new_price = (self.price / self.old_price) * 100
+        return new_price
+
+
+class ProductImages(models.Model):
+    images = models.ImageField(upload_to="product-images", default="product.jpg")
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Products Images"
+
+
+################### Cart, Order, OrderItems and Address ###################
+################### Cart, Order, OrderItems and Address ###################
+################### Cart, Order, OrderItems and Address ###################
+class CartOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default="1.99")
+    paid_status = models.BooleanField(default=False)
+    order_date = models.DateTimeField(auto_now_add=True)
+    product_status = models.CharField(choices=STATUS_CHOICE, max_length=30, default="processing")
+
+    class Meta:
+        verbose_name_plural = "Cart Orders"
+
+
+class CartOrderItems(models.Model):
+    order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+    product_status = models.CharField(max_length=200)
+    item = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default="1.99")
+    total = models.DecimalField(max_digits=10, decimal_places=2, default="1.99")
+
+    class Meta:
+        verbose_name_plural = "Cart Order Items"
+
+    def order_image(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" />' % self.image)
+
+
+################### Product Review, Wishlist, Address ###################
+################### Product Review, Wishlist, Address ###################
+################### Product Review, Wishlist, Address ###################
+
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    review = models.TextField()
+    rating = models.IntegerField(choices=RATING, default=None)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Product Reviews"
+
+    # Display categories image in the admin panel
+    # String representation in the database
+    def __str__(self):
+        return self.product.title
+
+    def get_rating(self):
+        return self.rating
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Wishlists"
+
+    # Display categories image in the admin panel
+    # String representation in the database
+    def __str__(self):
+        return self.product.title
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=100, null=True)
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Addresses"
