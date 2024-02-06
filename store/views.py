b7402e78d5ee5080b9294b8bc3913469b47f6431
officialjwise <officialjwise20@gmail.com>
2024-02-06 02:21:26 +0000

    Added vendor field to the product

--- store/views.py
+++ store/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from store.models import (
+    Category,
+    # Tags,
+    Vendor,
+    Product,
+    ProductImages,
+    CartOrder,
+    CartOrderItems,
+    ProductReview,
+    Wishlist,
+    Address,
+)
 
 
 def index(request):
-    return render(request, 'store/index.html')
+    product = Product.objects.all().order_by("-id")
+
+    context = {
+        "products" : product
+    }
+    return render(request, 'store/index.html', context)
