0e8601db2876dbf86465858be54c49356dd135e5
officialjwise <officialjwise20@gmail.com>
2024-02-06 13:15:40 +0000

    - Added product listing to the homepage dynamically from the database

--- store/views.py
+++ store/views.py
@@ -15,8 +15,24 @@ from store.models import (
 
 def index(request):
     # product = Product.objects.all().order_by("-id")
-    product = Product.objects.filter(product_status= "published", featured=True).order_by("-id")
+    product = Product.objects.filter(product_status="published", featured=True).order_by("-id")
     context = {
-        "products" : product
+        "products": product
     }
     return render(request, 'store/index.html', context)
+
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published")
+    context = {
+        "products": products
+    }
+    return render(request, 'store/product-list.html', context)
+
+
+def category_list_view(request):
+    categories = Category.objects.all()
+    context = {
+        "categories": categories
+    }
+    return render(request, 'store/category-list.html', context)
