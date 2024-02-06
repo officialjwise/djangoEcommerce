d449f0b767c611c11a45b260122ac3dd153b6088
officialjwise <officialjwise20@gmail.com>
2024-02-06 00:46:19 +0000

    Configured Django admin panel for the e-commerce store models. Registered models in admin.py with custom admin classes to enhance visualization and management. Added inlines for ProductImages within the Product admin for streamlined image management. Corrected 'list_display' attribute in CartOrderItemsAdmin to resolve 'admin.E108' error. The admin.py file is now optimized for effective administration of the e-commerce store.

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import (
+    Category,
+    Tags,
+    Vendor,
+    Product,
+    ProductImages,
+    CartOrder,
+    CartOrderItems,
+    ProductReview,
+    Wishlist,
+    Address,
+)
+
+
+class ProductImagesAdminInline(admin.TabularInline):
+    model = ProductImages
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesAdminInline]
+    list_display = ['user', 'title', 'product_image', 'price', 'featured', 'product_status']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category_image']
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['title', 'vendor_image']
+
+
+class CartOrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'price', 'paid_status', 'order_date', 'product_status']
+
+
+class CartOrderItemsAdmin(admin.ModelAdmin):
+    list_display = ['order', 'invoice_number', 'item', 'image', 'quantity', 'price', 'total']
+
+
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'review', 'rating']
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'date']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'address', 'status']
+
+
+# Registering the models with custom admin classes
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tags)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(CartOrder, CartOrderAdmin)
+admin.site.register(CartOrderItems, CartOrderItemsAdmin)
+admin.site.register(ProductReview, ProductReviewAdmin)
+admin.site.register(Wishlist, WishlistAdmin)
+admin.site.register(Address, AddressAdmin)
